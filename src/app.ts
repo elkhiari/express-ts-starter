5df82dcc863021e920816994b0d8fa776a9b210a
achraf360 <mouissa.achraf@gmail.com>
2024-09-25 15:53:34 +0000

    push

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,31 @@
 import express from "express";
-import { getIpAddresses } from "./utils/ipadress"
-import logger from './utils/logger';
-import dotenv from "dotenv"
-dotenv.config()
+import { getIpAddresses } from "./utils/ipadress";
+import logger from "./utils/logger";
+import dotenv from "dotenv";
+dotenv.config();
 import { connectToDatabase } from "./config/database";
-const ipAddresses = getIpAddresses()
+import path from "path";
+const ipAddresses = getIpAddresses();
 const PORT = parseInt(process.env.PORT as string, 10) || 8000;
 const app = express();
-app.use(express.json())
-app.get('*',(req,res)=> {
-    res.json({
-        env : process.env
-    })
-})
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
-
-const server = async() => {
-    logger.info(`Starting ${process.env.npm_package_name} (v${process.env.npm_package_version}) in ${process.env.NODE_ENV} mode`);
-    await connectToDatabase()
-    app.listen(PORT, '0.0.0.0', () => {
-        logger.info(`Server is running on http://localhost:${PORT}`)
-        for (const [name, addresses] of Object.entries(ipAddresses)) {
-          addresses.forEach(address => {
-            logger.info(`Server is accessible in Network at http://${address}:${PORT}`)
-          });
-        }
+const server = async () => {
+  logger.info(
+    `Starting ${process.env.npm_package_name} (v${process.env.npm_package_version}) in ${process.env.NODE_ENV} mode`
+  );
+  await connectToDatabase();
+  app.listen(PORT, "0.0.0.0", () => {
+    logger.info(`Server is running on http://localhost:${PORT}`);
+    for (const [name, addresses] of Object.entries(ipAddresses)) {
+      addresses.forEach((address) => {
+        logger.info(
+          `Server is accessible in Network at http://${address}:${PORT}`
+        );
       });
-}
+    }
+  });
+};
 
-export default server
+export default server;
